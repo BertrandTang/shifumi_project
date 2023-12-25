2da1e8f981fff419804366c64a6d3e48dc078646
Bertrand Tang <bertrand.tang.pro@gmail.com>
2023-12-25 22:10:25 +0100

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+var nameModal = document.getElementById("modal");
+var nameForm = document.getElementById("playeName_form");
+var playerName_input = document.getElementById("playerName");
+var gameContent = document.getElementById("gameContent");
+})
